64842d5b707b51ba0d3129ab5211b17ea621d19b
Chris Birk <cmbirk@gmail.com>
2014-12-24 21:48:01 -0500

    trying to load index.html with sub-templates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,25 @@
 var imports = [
   'euchre.directives',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.router'
 ];
 
 var app = angular.module('euchre', imports);
+
+//Angular Routing
+app.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider){
+    $stateProvider
+    .state('index', {
+      url: "/",
+      controller: "HomePageController",
+      templateUrl: "templates/homepage.html",
+      data: {title: "Euchre.io"}
+    });
+    // .state('profile-page', {
+    //   url: "/user/{id}",
+    //   controller: "ProfilePageController",
+    //   templateUrl: "templates/profile.html"
+    // });
+  }
+]);
---
+++ assets/js/controllers.js
@@ -0,0 +1,6 @@
+angular.module('euchre.controllers', [])
+  .controller('HomePageController', [
+    function () {
+      console.log("Home Page Controller loaded!");
+    }
+  ]);
